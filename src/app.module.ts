cda044e750a4b65260b82ab039074dad389f4c06
chohon <siaksiakx@gmail.com>
2023-11-18 21:46:15 +0900

    PostgreSQL DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import {
   NestModule,
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NextFunction, Request, Response } from 'express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -29,6 +30,17 @@ export class LoggerMiddleware implements NestMiddleware {
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
+      synchronize: true,
+      logging: true,
+      entities: [],
     }),
   ],
   controllers: [AppController],
