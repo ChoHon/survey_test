9738bb6c362e82236e70c5e241e2e17d3c41d583
chohon <siaksiakx@gmail.com>
2023-11-18 15:03:30 +0900

    HTTP 로그 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  MiddlewareConsumer,
+  Module,
+  NestMiddleware,
+  NestModule,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { NextFunction, Request, Response } from 'express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+  use(req: Request, res: Response, next: NextFunction) {
+    const { ip, method, originalUrl } = req;
+    res.on('finish', () => {
+      const { statusCode } = res;
+      this.logger.log(`${method} ${statusCode} - ${originalUrl} - ${ip}`);
+    });
+    next();
+  }
+}
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
