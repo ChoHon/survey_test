2e1ca686959d8a923bd33daf4218085d76bc6323
chohon <siaksiakx@gmail.com>
2023-11-24 22:50:43 +0900

    설문지(form) entity에 status 컬럼 추가

--- src/form/entities/form.entity.ts
+++ src/form/entities/form.entity.ts
@@ -1,8 +1,21 @@
-import { ObjectType, Field, InputType } from '@nestjs/graphql';
+import {
+  ObjectType,
+  Field,
+  InputType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { Answer } from 'src/answer/entities/answer.entity';
 import { Common } from 'src/common/entities/common.entity';
 import { QuestionInForm } from 'src/question-form/entities/question-form.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
+
+export enum FormStatus {
+  STANDBY = 0,
+  ONGOING = 1,
+  COMPLETE = 2,
+}
+
+registerEnumType(FormStatus, { name: 'FormStatus' });
 
 @InputType({ isAbstract: true })
 @Entity()
@@ -15,6 +28,10 @@ export class Form extends Common {
   @Column({ type: 'text', default: '' })
   @Field(() => String)
   description: string;
+
+  @Column({ type: 'enum', enum: FormStatus, default: FormStatus.STANDBY })
+  @Field(() => FormStatus)
+  status: FormStatus;
 
   @OneToMany(() => QuestionInForm, (qf) => qf.form)
   @Field(() => [QuestionInForm])
